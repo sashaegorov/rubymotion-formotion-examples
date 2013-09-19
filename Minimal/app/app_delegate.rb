d662480a7ccbe774a4f01d20a972c1ed8bbb28c2
Sasha Egorov <a.a.egoroff@gmail.com>
2013-09-20 00:01:53 +0400

    For motion added

--- Minimal/app/app_delegate.rb
+++ Minimal/app/app_delegate.rb
@@ -1,5 +1,36 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    true
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    # Prepare form
+    @form = Formotion::Form.new({
+      sections: [{
+        title: "Sign up",
+        rows: [{
+          title: "Email address",
+          key: :email,
+          type: :email,
+          auto_correction: :no,
+          auto_capitalization: :none
+          },{
+          title: "Password",
+          subtitle: "8 or more characters",
+          key: :password,
+          type: :string,
+          secure: true
+          }]
+      }, {
+        rows: [{
+          title: "Join now",
+          type: :submit
+        }]
+      }]
+    })
+
+    @form_controller = Formotion::FormController.alloc.initWithForm(@form)
+    @window.rootViewController = @form_controller
+    @window.makeKeyAndVisible
+
   end
 end
